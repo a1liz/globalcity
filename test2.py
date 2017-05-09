077ff2aecbafe7577bdc09c84f544be7037bed17
王理治 <liz@lizs-mbp.local>
2017-05-09 20:35:40 +0800

    更改了爬虫文件

--- test2.py
+++ test2.py
@@ -14,6 +14,8 @@ while i != 10 :
         for key in data :
             for city in data[key] :
                 if key == u'China' :
+                    if city == u"" :
+                        continue
                     with open('newcountry.json','r') as ff:
                         data1=json.load(ff)
                     if city in data1:
@@ -32,11 +34,12 @@ while i != 10 :
                     print city
                     if count %10 == 0:
                         print count
+                        time.sleep(3)
                     readed = json.load(open('newcountry.json','r'))
                     readed[city] = ((location.latitude,location.longitude))
                     json.dump(readed,open('newcountry.json','w'))
                     if count%50 == 0 :
-                        time.sleep(1)
+                        time.sleep(5)
     except Exception,e:
         print str(e)
         time.sleep(5)
