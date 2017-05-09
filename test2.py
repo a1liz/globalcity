9e47b302223c9e44db84329376e144af1a93ff81
a1liz <694426737@qq.com>
2017-05-09 13:35:22 +0800

    修改了爬虫文件，爬取了中国部分所有城市并将其分类成已查找，错误城市，未找到三个json文件

--- test2.py
+++ test2.py
@@ -7,18 +7,28 @@ with open('countriesToCities.json', 'r') as f:
     data = json.load(f)
 
 count = 0
+passcount = 0
 i = 0
-while i != 10 :
+while i != 1 :
     try :
         i+=1
         for key in data :
             for city in data[key] :
                 if key == u'China' :
                     if city == u"" :
+                        print "have null city"
+                        time.sleep(5)
                         continue
                     with open('newcountry.json','r') as ff:
                         data1=json.load(ff)
+                    data1 = json.load(open('newcountry.json','r'))
+                    data2 = json.load(open('errorcountry.json','r'))
+                    data3 = json.load(open('passcountry.json','r'))
                     if city in data1:
+                        continue
+                    if city in data2:
+                        continue
+                    if city in data3:
                         continue
                     try :
                         citystring = city.encode()
@@ -27,11 +37,19 @@ while i != 10 :
                         readed[city] = city
                         json.dump(readed,open('errorcountry.json','w'))
                         continue
+                    print citystring
                     location = geolocator.geocode(citystring)
                     if location == None:
+                        passcount += 1
+                        if passcount % 50 == 0 :
+                            print passcount
+                            time.sleep(5)
+                        print "the location is null"
+                        readed = json.load(open('passcountry.json','r'))
+                        readed[city] = city
+                        json.dump(readed,open('passcountry.json','w'))
                         continue
                     count += 1
-                    print city
                     if count %10 == 0:
                         print count
                         time.sleep(3)
